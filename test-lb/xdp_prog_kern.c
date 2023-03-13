1e241025c5a62f2177cdcd09e82ed5aae4fcfe85
acm19 <acm19@ic.ac.uk>
2023-03-13 23:06:09 +0000

    fix udp checksum

--- test-lb/xdp_prog_kern.c
+++ test-lb/xdp_prog_kern.c
@@ -6,21 +6,42 @@
 #include "../common/parsing_helpers.h"
 #include "../common/rewrite_helpers.h"
 
-#define MAX_OPT_WORDS 10 // 40 bytes for options
-#define MAX_TARGET_COUNT 64 // max number of target servers for LB
-#define REDIR_OPT_TYPE 42
-#define MAX_UDP_SIZE 1480
-
-#ifndef memcpy
-#define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
-#endif
-
 static __always_inline __u16 csum_reduce_helper(__u32 csum)
 {
 	csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
 	csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
 
 	return csum;
+}
+
+static inline unsigned short generic_checksum(unsigned short *buf, void *data_end, unsigned long sum, int max) {
+    
+    for (int i = 0; i < max; i += 2) {
+	if ((void *)(buf + 1) > data_end)
+	    break;
+        sum += *buf;
+        buf++;
+    }
+
+    if((void *)buf +1 <= data_end) {
+	sum +=  bpf_htons((*((unsigned char *)buf)) << 8);
+    }
+
+    sum = (sum & 0xffff) + (sum >> 16);
+    sum = (sum & 0xffff) + (sum >> 16);
+    return ~sum;
+}
+
+static inline __u16 l4_checksum(struct iphdr *iph, void *l4, void *data_end)
+{
+    __u32 csum = 0;
+    csum += *(((__u16 *) &(iph->saddr))+0); // 1st 2 bytes
+    csum += *(((__u16 *) &(iph->saddr))+1); // 2nd 2 bytes
+    csum += *(((__u16 *) &(iph->daddr))+0); // 1st 2 bytes
+    csum += *(((__u16 *) &(iph->daddr))+1); // 2nd 2 bytes
+    csum += bpf_htons((__u16)iph->protocol); // protocol is a u8
+    csum += bpf_htons((__u16)(data_end - (void *)l4)); 
+    return generic_checksum((unsigned short *) l4, data_end, csum, 1480);
 }
 
 SEC("xdp")
@@ -79,6 +100,7 @@ int  xdp_prog_simple(struct xdp_md *ctx)
 
 		//TODO:
 		udph->check = 0;
+		udph->check = l4_checksum(iph, udph, data_end);
 
 		action = XDP_TX;
 	}
@@ -93,8 +115,6 @@ int  xdp_prog_tcp(struct xdp_md *ctx)
 	void *data = (void *)(long)ctx->data;
 	struct ethhdr *ethh;
 	struct iphdr *iph;
-	__u32 size_ip = sizeof(struct iphdr);
-	__u32 size_tcp = sizeof(struct tcphdr);
 	struct tcphdr *tcph;
 	__u32 action = XDP_DROP; /* Default action */
 	struct hdr_cursor nh;
@@ -127,9 +147,18 @@ int  xdp_prog_tcp(struct xdp_md *ctx)
 	}
 	bpf_printk("parsed TCP header");
 
-	if (bpf_ntohs(tcph->dest) == 4172) {
-		bpf_printk("DST will be 4173");
-		tcph->dest = bpf_htons(bpf_ntohs(tcph->dest)+1);
+	if (bpf_ntohs(tcph->dest) == 4172 || bpf_ntohs(tcph->dest) == 4173) {
+		if (bpf_ntohs(tcph->dest) == 4172) {
+
+			bpf_printk("DST will be 4173");
+			tcph->dest = bpf_htons(bpf_ntohs(tcph->dest)+1);
+
+		} else if (bpf_ntohs(tcph->dest) == 4173) {
+
+			bpf_printk("DST will be 4173");
+			tcph->dest = bpf_htons(bpf_ntohs(tcph->dest)-1);
+
+		}
 
 		swap_src_dst_ipv4(iph);
 		bpf_printk("Swapped ip addresses");
@@ -137,14 +166,14 @@ int  xdp_prog_tcp(struct xdp_md *ctx)
 		swap_src_dst_mac(ethh);
 		bpf_printk("Swapped eth addresses");
 
-		bpf_printk("destionation udp port after is %u", bpf_ntohs(tcph->dest));
+		bpf_printk("destionation TCP port after is %u", bpf_ntohs(tcph->dest));
 
 		iph->check = 0;
-		iph->check = ~csum_reduce_helper(bpf_csum_diff(0, 0, (__be32 *)iph, size_ip, 0));
+		iph->check = ~csum_reduce_helper(bpf_csum_diff(0, 0, (__be32 *)iph, sizeof(struct iphdr), 0));
 
 		//TODO: fix tcp checksum
 		tcph->check = 0;
-		tcph->check = ~csum_reduce_helper(bpf_csum_diff(0, 0, (__be32 *)tcph, size_tcp, 0));
+		tcph->check = l4_checksum(iph, tcph, data_end);
 
 		action = XDP_TX;
 	}

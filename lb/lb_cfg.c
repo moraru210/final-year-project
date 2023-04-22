a7a795ed7098f4c13732f943d3e476d28dc13db4
acm19 <acm19@ic.ac.uk>
2023-04-22 19:52:13 +0100

    create lb configurer file in userspace

---
+++ lb/lb_cfg.c
@@ -0,0 +1,225 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+static const char *__doc__ = "XDP redirect helper\n"
+	" - Allows to populate/query tx_port and redirect_params maps\n";
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
+
+#include <locale.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf_endian.h>
+
+#include <net/if.h>
+#include <linux/if_ether.h>
+#include <linux/if_link.h> /* depend on kernel-headers installed */
+
+#include "../common/common_params.h"
+#include "../common/common_user_bpf_xdp.h"
+#include "../common/common_libbpf.h"
+
+#include "../common/xdp_stats_kern_user.h"
+
+#include "./common.h"
+
+static const struct option_wrapper long_options[] = {
+
+	{{"help",        no_argument,		NULL, 'h' },
+	 "Show help", false},
+
+	{{"dev",         required_argument,	NULL, 'd' },
+	 "Operate on device <ifname>", "<ifname>", true},
+
+	{{"redirect-dev",         required_argument,	NULL, 'r' },
+	 "Redirect to device <ifname>", "<ifname>", true},
+
+	{{"src-mac", required_argument, NULL, 'L' },
+	 "Source MAC address of <dev>", "<mac>", true },
+
+	{{"dest-mac", required_argument, NULL, 'R' },
+	 "Destination MAC address of <redirect-dev>", "<mac>", true },
+
+	{{"quiet",       no_argument,		NULL, 'q' },
+	 "Quiet mode (no output)"},
+
+	{{0, 0, NULL,  0 }, NULL, false}
+};
+
+static int parse_u8(char *str, unsigned char *x)
+{
+	unsigned long z;
+
+	z = strtoul(str, 0, 16);
+	if (z > 0xff)
+		return -1;
+
+	if (x)
+		*x = z;
+
+	return 0;
+}
+
+static int parse_mac(char *str, unsigned char mac[ETH_ALEN])
+{
+	if (parse_u8(str, &mac[0]) < 0)
+		return -1;
+	if (parse_u8(str + 3, &mac[1]) < 0)
+		return -1;
+	if (parse_u8(str + 6, &mac[2]) < 0)
+		return -1;
+	if (parse_u8(str + 9, &mac[3]) < 0)
+		return -1;
+	if (parse_u8(str + 12, &mac[4]) < 0)
+		return -1;
+	if (parse_u8(str + 15, &mac[5]) < 0)
+		return -1;
+
+	return 0;
+}
+
+static int write_iface_params(int map_fd, unsigned char *src, unsigned char *dest)
+{
+	if (bpf_map_update_elem(map_fd, src, dest, 0) < 0) {
+		fprintf(stderr,
+			"WARN: Failed to update bpf map file: err(%d):%s\n",
+			errno, strerror(errno));
+		return -1;
+	}
+
+	printf("forward: %02x:%02x:%02x:%02x:%02x:%02x -> %02x:%02x:%02x:%02x:%02x:%02x\n",
+			src[0], src[1], src[2], src[3], src[4], src[5],
+			dest[0], dest[1], dest[2], dest[3], dest[4], dest[5]
+	      );
+
+	return 0;
+}
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+const char *pin_basedir =  "/sys/fs/bpf";
+
+int main(int argc, char **argv)
+{
+	int i;
+	int len;
+	int map_fd;
+	bool redirect_map;
+	char pin_dir[PATH_MAX];
+	unsigned char src[ETH_ALEN];
+	unsigned char dest[ETH_ALEN];
+
+	struct config cfg = {
+		.ifindex   = -1,
+		.redirect_ifindex   = -1,
+	};
+
+	/* Cmdline options can change progsec */
+	parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
+
+	redirect_map = (cfg.ifindex > 0) && (cfg.redirect_ifindex > 0);
+
+	if (cfg.redirect_ifindex > 0 && cfg.ifindex == -1) {
+		fprintf(stderr, "ERR: required option --dev missing\n\n");
+		usage(argv[0], __doc__, long_options, (argc == 1));
+		return EXIT_FAIL_OPTION;
+	}
+
+	len = snprintf(pin_dir, PATH_MAX, "%s/%s", pin_basedir, cfg.ifname);
+	if (len < 0) {
+		fprintf(stderr, "ERR: creating pin dirname\n");
+		return EXIT_FAIL_OPTION;
+	}
+
+	printf("map dir: %s\n", pin_dir);
+
+	/* Open the tx_port map corresponding to the cfg.ifname interface */
+	/****************************************************/
+	map_fd = open_bpf_map_file(pin_dir, "ports_map", NULL);
+	if (map_fd < 0) {
+		return EXIT_FAIL_BPF;
+	}
+
+	unsigned int query_port = 4172;
+	unsigned int result_port;
+
+	int err = bpf_map_lookup_elem(map_fd, &query_port, &result_port);
+	if (err < 0) {
+		printf("failed finding port number");
+		return EXIT_FAIL_BPF;
+	} else {
+		printf("client port: %u\n", result_port);
+	}
+
+	query_port = 4173;
+	result_port;
+
+	err = bpf_map_lookup_elem(map_fd, &query_port, &result_port);
+	if (err < 0) {
+		printf("failed finding port number");
+		return EXIT_FAIL_BPF;
+	} else {
+		printf("client port: %u\n", result_port);
+	}
+
+	/****************************************************/
+	// map_fd = open_bpf_map_file(pin_dir, "ack_map", NULL);
+	// if (map_fd < 0) {
+	// 	return EXIT_FAIL_BPF;
+	// }
+
+	// struct connection conn1;
+	// conn1.dst_port = 59668;
+	// conn1.src_port = 4172;
+
+	// unsigned int val1;
+
+	// err = bpf_map_lookup_elem(map_fd, &conn1, &val1);
+	// if (err < 0) {
+	// 	printf("failed finding ack number");
+	// 	return EXIT_FAIL_BPF;
+	// } else {
+	// 	printf("conn1 ack val: %u\n", val1);
+	// }
+
+	// struct connection conn2;
+	// conn2.dst_port = 56412;
+	// conn2.src_port = 4173;
+
+	// unsigned int val2;
+
+	// err = bpf_map_lookup_elem(map_fd, &conn2, &val2);
+	// if (err < 0) {
+	// 	printf("failed finding ack number");
+	// 	return EXIT_FAIL_BPF;
+	// } else {
+	// 	printf("conn1 ack val: %u\n", val2);
+	// }
+	/****************************************************/
+
+	// map_fd = open_bpf_map_file(pin_dir, "seq_offsets", NULL);
+	// if (map_fd < 0) {
+	// 	return EXIT_FAIL_BPF;
+	// }
+
+	// struct offset_key query_offset;
+	// query_offset.new_port = 4172;
+	// query_offset.original_port = 4173;
+	// err = bpf_map_delete_elem(map_fd, &query_offset);
+	// if (err < 0) {
+	// 	printf("failed to delete offset_key from map\n");
+	// } else {
+	// 	printf("successfully deleted offset_key form map\n");
+	// }
+
+	return EXIT_OK;
+}

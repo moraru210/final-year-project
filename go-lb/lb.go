c741a556fd80b36e2528a906a36277e12278150f
acm19 <acm19@ic.ac.uk>
2023-04-29 17:47:19 +0100

    update go-lb to access ports_map and add conns

--- go-lb/lb.go
+++ go-lb/lb.go
@@ -11,8 +11,7 @@ import (
 )
 
 func main() {
-
-    connection_map_path := "/sys/fs/bpf/test/ports_map"
+    const connection_map_path = "/sys/fs/bpf/test/ports_map"
     conn_map_fd, err := bpf.ObjGet(connection_map_path)
     if (err != nil) {
         fmt.Println("Error finding map object: ", err.Error())
@@ -20,122 +19,112 @@ func main() {
     }
     fmt.Println("complete finding map")
 
-    client_conn1 := C.struct_connection{
-        src_port: 4000,
-        dst_port: 8080,
-    }
-
-    worker_conn1 := C.struct_connection{
-        src_port: 8080,
-        dst_port: 4171,
-    }
-
-
-    err = bpf.UpdateElement(conn_map_fd, "ports_map", unsafe.Pointer(&client_conn1), unsafe.Pointer(&worker_conn1), bpf.BPF_ANY)
-    if (err != nil) {
-        fmt.Println("Error in updating map: ", err.Error())
-        return
-    } 
-    fmt.Println("complete updating map")
-    
-    var worker_conn_val = &C.struct_connection{
-        src_port: 0,
-        dst_port: 0,
-    }
-    err = bpf.LookupElement(conn_map_fd, unsafe.Pointer(&client_conn1), unsafe.Pointer(worker_conn_val))
-    if (err != nil) {
-        fmt.Println("Error in lookup map: ", err.Error())
-        return
-    }
-    fmt.Println("complete lookup map")
-
-    fmt.Println("dst_port of worker conn is %d\n", worker_conn_val.dst_port)
-
     //set up connection with worker nodes
-    // setUpWorkerConnections()
+    conn1, conn2, err := setUpWorkerConnections()
+    if (err != nil) {
+        fmt.Println("Error setting up worker connections:", err.Error())
+        return
+    }
+    defer conn1.Close()
+    defer conn2.Close()
 
-    // // Listen for incoming connections
-    // ln, err := net.Listen("tcp", ":8080")
-    // if err != nil {
-    //     fmt.Println("Error listening:", err.Error())
-    //     return
-    // }
-    // defer ln.Close()
+    // Listen for incoming connections
+    ln, err := net.Listen("tcp", ":8080")
+    if err != nil {
+        fmt.Println("Error listening:", err.Error())
+        return
+    }
+    defer ln.Close()
 
-    // fmt.Println("Server listening on port 8080")
+    fmt.Println("Server listening on port 8080")
 
-    // const num_workers = 2;
-    // rr := 0
-    // for {
-    //     // Accept new connection
-    //     conn, err := ln.Accept()
-    //     if err != nil {
-    //         fmt.Println("Error accepting:", err.Error())
-    //         continue
-    //     }
+    const num_workers = 2;
+    rr := 1
+    for {
+        // Accept new connection
+        conn, err := ln.Accept()
+        if err != nil {
+            fmt.Println("Error accepting:", err.Error())
+            continue
+        }
 
-    //     // Handle new connection in a goroutine
-    //     go handleConnection(conn)
-    // }
+        // Handle new connection in a goroutine
+        if (rr % num_workers == 1) {
+            go handleConnection(conn, conn1, conn_map_fd)
+        } else {
+            go handleConnection(conn, conn2, conn_map_fd)
+        }
+    }
 }
 
-func setUpWorkerConnections() (net.Conn, net.Conn) {
+func setUpWorkerConnections() (net.Conn, net.Conn, error) {
     // Connect to netcat server at localhost:4170
     conn1, err := net.Dial("tcp", "localhost:4170")
     if err != nil {
         fmt.Println("Error connecting to localhost:4170:", err)
-        return nil, nil
+        return nil, nil, err
     }
-
     fmt.Println("Connected to localhost:4170")
 
     // Connect to netcat server at localhost:4171
     conn2, err := net.Dial("tcp", "localhost:4171")
     if err != nil {
         fmt.Println("Error connecting to localhost:4171:", err)
-        return nil, nil
+        return nil, nil, err
     }
-
     fmt.Println("Connected to localhost:4171")
-
-    // Send messages to the netcat servers
-    message1 := "Hello from connection 1\n"
-    message2 := "Hello from connection 2\n"
-
-    _, err = conn1.Write([]byte(message1))
-    if err != nil {
-        fmt.Println("Error sending message to localhost:4170:", err)
-        return nil, nil
-    }
-
-    fmt.Println("Sent message to localhost:4170")
-
-    _, err = conn2.Write([]byte(message2))
-    if err != nil {
-        fmt.Println("Error sending message to localhost:4171:", err)
-        return nil, nil
-    }
-
-    fmt.Println("Sent message to localhost:4171")
-    return conn1, conn2
+    
+    return conn1, conn2, nil
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, conn_w net.Conn, conn_map_fd int) {
     defer conn.Close()
 
     // Get the client's IP address and port number
     remoteAddr := conn.RemoteAddr().(*net.TCPAddr)
-    ip := remoteAddr.IP.String()
-    port := C.uint(remoteAddr.Port)
+    c_ip := remoteAddr.IP.String()
+    c_port := C.uint(remoteAddr.Port)
 
-    fmt.Printf("Client connected from %s:%d\n", ip, port)
+    fmt.Printf("Client connected from %s:%d\n", c_ip, c_port)
 
-    c := C.struct_connection{
-        src_port: port,
+    client_c := C.struct_connection{
+        src_port: c_port,
         dst_port: 8080,
     }
 
-    fmt.Printf("Access connection struct src_port %d\n", c.src_port);
+    fmt.Printf("Access client connection struct src_port %d\n", client_c.src_port);
+
+    // Get the worker's IP address and port number
+    remoteAddr = conn_w.RemoteAddr().(*net.TCPAddr)
+    w_ip := remoteAddr.IP.String()
+    w_port := C.uint(remoteAddr.Port)
+
+    fmt.Printf("Client connected from %s:%d\n", w_ip, w_port)
+
+    worker_c := C.struct_connection{
+        src_port: 8080,
+        dst_port: w_port,
+    }
+
+    fmt.Printf("Access worker connection struct dst_port %d\n", worker_c.dst_port);
+
+    // Update Ports Map with conn->conn_w and rev(conn_w)->rev(conn)
+    err := bpf.UpdateElement(conn_map_fd, "ports_map", unsafe.Pointer(&client_c), unsafe.Pointer(&worker_c), bpf.BPF_ANY)
+    if (err != nil) {
+        fmt.Println("Error in updating map: ", err.Error())
+        return
+    } 
+    fmt.Println("complete updating map")
+
+    r_client := reverse(client_c)
+    r_worker := reverse(worker_c)
+
+    err = bpf.UpdateElement(conn_map_fd, "ports_map", unsafe.Pointer(&r_worker), unsafe.Pointer(&r_client), bpf.BPF_ANY)
+    if (err != nil) {
+        fmt.Println("Error in updating map: ", err.Error())
+        return
+    } 
+    fmt.Println("complete updating map")
 
     // Receive messages from the client
     buffer := make([]byte, 1024)
@@ -149,4 +138,11 @@ func handleConnection(conn net.Conn) {
         fmt.Printf("Received message: %s\n", buffer[:n])
         buffer = make([]byte, 1024)
     }
+}
+
+func reverse(conn C.struct_connection) C.struct_connection {
+    var tmp = conn.src_port
+    conn.src_port = conn.dst_port
+    conn.dst_port = tmp
+    return conn
 }

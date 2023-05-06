2510eb9a109bf57c785fb96a434b78e40845c0df
acm19 <acm19@ic.ac.uk>
2023-05-07 00:01:11 +0100

    change directy of setup script

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pwd
-cd /home/moraru210/Documents/eBPF/final-year-project/lb
+cd /home/moraru210/Documents/eBPF/final-year-project/kernel
 ../testenv/testenv.sh setup -n test --legacy-ip
 ../testenv/testenv.sh exec -n test -- ./xdp_loader -d veth0 -F --progsec xdp_pass
 ../testenv/testenv.sh load -n test -- -F --progsec xdp_tcp

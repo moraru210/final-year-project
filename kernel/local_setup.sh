749dc5bff9bea23a55e60b9ded14b9bfc544fae5
moraru210 <alexandru210moraru@gmail.com>
2023-05-10 21:55:10 +0100

    upgrade local_setup to handle map reuse

--- kernel/local_setup.sh
+++ kernel/local_setup.sh
@@ -4,4 +4,21 @@ set -o errexit
 
 pwd
 cd /home/moraru210/Documents/eBPF/final-year-project/kernel
-./xdp_loader -d lo -F --progsec xdp_tcp --auto-mode
+
+REUSE=false
+
+# parse command line options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--reuse) REUSE=true; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# run xdp_loader with the appropriate options
+if [ "$REUSE" = true ]; then
+    ./xdp_loader -d lo -F --progsec xdp_tcp --auto-mode --reuse
+else
+    ./xdp_loader -d lo -F --progsec xdp_tcp --auto-mode
+fi

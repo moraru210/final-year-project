954475e283cb9733002a76b642282aee91139a7e
moraru210 <alexandru210moraru@gmail.com>
2023-05-27 00:00:31 +0100

    add updated scripts using xdp-tools/xdp-loader

--- kernel/local_setup.sh
+++ kernel/local_setup.sh
@@ -3,22 +3,6 @@
 set -o errexit
 
 pwd
-cd /home/moraru210/Documents/eBPF/final-year-project/kernel
+cd /home/moraru210/Desktop/final-year-project/kernel
 
-REUSE=false
-
-# parse command line options
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -r|--reuse) REUSE=true; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
-# run xdp_loader with the appropriate options
-if [ "$REUSE" = true ]; then
-    ./xdp_loader -d lo -F --progsec xdp_tcp --auto-mode --reuse
-else
-    ./xdp_loader -d lo -F --progsec xdp_tcp --auto-mode
-fi
+../xdp-tools/xdp-loader/xdp-loader load -m skb lo xdp_prog_kern.o --pin-path /sys/fs/bpf/lo -v -s xdp_tcp
---
+++ kernel/local_status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -o errexit
+
+pwd
+cd /home/moraru210/Desktop/final-year-project/kernel
+
+../xdp-tools/xdp-loader/xdp-loader status

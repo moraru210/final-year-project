0011055066c4fb9bd689297818cd486118e6f152
moraru210 <alexandru210moraru@gmail.com>
2023-05-26 15:44:05 +0100

    cleanup

--- kernel/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-pwd
-cd /home/moraru210/Documents/eBPF/final-year-project/kernel
-../testenv/testenv.sh setup -n test --legacy-ip
-../testenv/testenv.sh exec -n test -- ./xdp_loader -d veth0 -F --progsec xdp_pass
-../testenv/testenv.sh load -n test -- -F --progsec xdp_tcp

08cfd7d3c7330b65b6ace8a2d1b154e4e532d8d1
moraru210 <alexandru210moraru@gmail.com>
2023-06-02 16:52:48 +0100

    custom port http server on localhost

--- benchmarks/http-listener.go
+++ benchmarks/http-listener.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +12,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//input parsing
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: test <integer> <integer>")
+		os.Exit(1)
+	}
+
 	http.HandleFunc("/", handler)
 
-	log.Println("Server running at http://localhost:4170/")
-	log.Fatal(http.ListenAndServe("localhost:4170", nil))
+	address := "localhost:" + os.Args[1]
+	log.Printf("Server running at http://%s/\n", address)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
